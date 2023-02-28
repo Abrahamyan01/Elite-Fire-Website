b19990cc20a1e302be10046a524a26d81143a432
Harutyun Abrahamyan <harutyun.abrahamyan00@gmail.com>
2023-02-28 16:47:35 +0400

    add files to repo

---
+++ main.js
@@ -0,0 +1,18 @@
+//Slider Big
+const bigSlider=new Swiper(".swiper",{
+    slidesPerView:1,
+    navigaton:{
+        nextEl:"swiper-button-next",
+        prevEl:"swiper-button-prev",
+    },
+
+});
+//Slider Panel
+const panelSlider=new Swiper(".panel_slider",{
+    slidesPerView:1,
+    navigaton:{
+        nextEl:"swiper-button-next",
+        prevEl:"swiper-button-prev",
+    },
+
+});
